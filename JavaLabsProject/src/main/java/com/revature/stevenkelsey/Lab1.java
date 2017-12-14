cc5720cc78ab90e460bf92e59b46be40f7893d77
Steven Kelsey <steven.kelsey@revature.com>
2017-12-14 16:36:39 -0500

    java labs project

---
+++ JavaLabsProject/src/main/java/com/revature/stevenkelsey/Lab1.java
@@ -0,0 +1,9 @@
+package com.revature.stevenkelsey;
+
+public class Lab1 {
+
+	/*
+	 * 
+	 * lab one code would be here
+	 */
+}
---
+++ JavaLabsProject/src/main/java/com/revature/stevenkelsey/Lab2.java
@@ -0,0 +1,7 @@
+package com.revature.stevenkelsey;
+
+public class Lab2 {
+	/*
+	 * 	Lab 2 code would be here
+	 */
+}

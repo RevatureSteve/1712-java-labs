db6768a2dba7f2e1540f94a7d1c63717ea311ac1
James Holzer <james.holzer1990@gmail.com>
2017-12-14 21:30:23 -0500

    Lab 1 and 2 with tester for 1

---
+++ src/test/java/com/revature/jamesholzer/TestLab2.java
@@ -0,0 +1,14 @@
+package com.revature.jamesholzer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestLab2 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

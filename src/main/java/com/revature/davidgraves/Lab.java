6ac93c2f805de06d0f84773d8d2f2fb7f3ded6e3
David Graves <davidjgrvs@gmail.com>
2017-12-15 11:13:58 -0500

    rearranging files

---
+++ src/main/java/com/revature/davidgraves/Lab.java
@@ -0,0 +1,71 @@
+package com.revature.davidgraves;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Lab {
+	
+	/*
+	  1. Return the nth fibonacci number
+	  f(0) = 0
+	  f(1) = 1
+	  f(10) = 55
+	*/
+	public static int fibonacci(int n) {
+		
+		
+	}
+	
+	/*
+	  2. Sort array of integers
+	  f([2,4,5,1,3,1]) = [1,1,2,3,4,5]
+	  Don't use built-in sort() method... that would be lame.
+	*/
+	public static int[] sort(int[] array) {
+		
+	}
+	
+	/*
+	  3. Return the factorial of n
+	  f(0) = 1
+	  f(1) = 1
+	  f(3) = 6
+	*/
+	public static int factorial(int n) {
+		
+	}
+	
+	/*
+	  4. Rotate left
+	  Given array, rotate left n times and return array
+	  f([1,2,3,4,5], 1) = [2,3,4,5,1]
+	  f([1,2,3,4,5], 6) = [2,3,4,5,1]
+	  f([1,2,3,4,5], 3) = [4,5,1,2,3]
+	*/
+	public static int[] rotateLeft(int[] array, int n) {
+		
+	}
+	
+	/*
+	  5. Balanced Brackets
+	  A bracket is any one of the following: (, ), {, }, [, or ]
+	  The following are balanced brackets:
+	    ()
+	    ()()
+	    (())
+	    ({[]})
+	  The following are NOT balanced brackets:
+	  (
+	  )
+	  (()
+	  ([)]
+	  Return true if balanced
+	  Return false if not balanced
+	*/
+	public static boolean balancedBrackets(String bracketsString) {
+		
+	}
+
+
+	
+}
